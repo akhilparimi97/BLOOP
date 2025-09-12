940df63930183ed41f2706c396122eecd4262ca4
akhilparimi97 <58124166+akhilparimi97@users.noreply.github.com>
2025-09-12 16:14:34 +0530

    Update GameManager.h

--- src/GameManager.h
+++ src/GameManager.h
@@ -6,7 +6,9 @@
 #define BUTTON_A 5
 #define BUTTON_B 6
 #else
-#include "bloop_adapter.h"   // brings in DisplayShim + BUTTON_A/B redefs
+#include "ArduinoShim.h"
+#define BUTTON_A 0
+#define BUTTON_B 1
 #endif
 
 // Constants
@@ -20,13 +22,13 @@
 #define DEBOUNCE_DELAY 200
 #define EXIT_HOLD_DURATION 1500
 
-// Game IDs for high score indexing
+// Game IDs
 enum GameID {
     GAME_SNAKE = 0,
     GAME_PONG = 1
 };
 
-// Input helper structure
+// Input helper
 struct InputState {
     bool buttonA;
     bool buttonB;
@@ -34,12 +36,8 @@ struct InputState {
     bool exitRequested;
 };
 
-#ifndef __EMSCRIPTEN__
+// Global objects
 extern Adafruit_SSD1306 display;
-#else
-extern DisplayShim display;
-#endif
-
 extern int highScores[NUM_GAMES];
 extern bool justWoke;
 
@@ -47,17 +45,17 @@ extern bool justWoke;
 void initGameManager();
 void runGameLoop();
 
-// UI functions
+// UI
 void drawStatusBar(const char* gameName, int currentScore, int highScore);
 void drawStatusBarMenu();
 void showExitHoldBar(float progress);
 
-// Input functions  
+// Input
 InputState getInputState();
 void waitForButtonRelease();
 bool buttonPressed(int pin);
 
-// Utility functions
+// Utility
 void clearPlayfield();
 void showGameOver(const char* gameName, int score, int highScore);
 void showGetReady(const char* gameName, const char* instructions = nullptr);
