eb19a013c9b792f434683f0cd0905bde4da36908
akhilparimi97 <58124166+akhilparimi97@users.noreply.github.com>
2025-09-12 16:37:36 +0530

    Update SnakeGame.h

--- src/SnakeGame.h
+++ src/SnakeGame.h
@@ -1,9 +1,6 @@
-// SnakeGame.h
-#ifndef SNAKE_GAME_H
-#define SNAKE_GAME_H
+#pragma once
+#include "GameManager.h"
 
-int playSnake();
-
-#endif
-
-// Pong.h  
+// Non-blocking per-frame snake
+void startSnake();
+bool stepSnake(int& outScore, bool& exitRequested, bool& gameOver);
